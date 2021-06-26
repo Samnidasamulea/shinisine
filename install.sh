124b883569efca22f21aafd6132e526674d78cec
Samnidasamulea <86532888+Samnidasamulea@users.noreply.github.com>
2021-06-27 05:17:12 +0700

    Update and rename run_tests.sh to install.sh

--- install.sh
+++ install.sh
@@ -15,11 +15,7 @@
 
 #   Description: unit and integration tests for cirque
 #   Usage: sudo ./run_tests.sh
-        sudo apt install curl gnupg
-        curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
-        sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
-        echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-
+lscpu
   #sleep 5
   # run_flask_virtual_home_test
   # testexit=$?
